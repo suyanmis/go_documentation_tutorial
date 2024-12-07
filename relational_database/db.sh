7943bc44cfee006a151b06245a472c56880b7966
suyanmis <serhatuyanmis@hotmail.com>
2024-12-08 04:04:11 +0800

    Relational DB implementation(incomplete)

---
+++ relational_database/db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+podman build -t example-go/mysql:latest .
+podman run --rm -d -p 3306:3306 --name=mysql mysql 
+
+sleep 7
+podman exec -it mysql mysql -u root -proot -e "USE recordings; SELECT * FROM album;"
